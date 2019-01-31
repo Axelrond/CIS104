db3e67b50cfb860d6cfb988f1aeb780bd2a42cf1
XËŁ <46701041+Axelrond@users.noreply.github.com>
2019-01-30 20:18:22 -0800

    Add files via upload

---
+++ primes/primes/primes.py
@@ -0,0 +1,19 @@
+primes = [2]
+
+
+
+for testNum in range(3, 1001):
+
+	for divisor in range(2, testNum):
+
+		if testNum % divisor == 0:
+
+			break
+
+	if not testNum % divisor == 0:
+
+		primes.append(testNum)
+
+
+
+print(primes)
